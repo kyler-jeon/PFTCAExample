05b8ec529d78d30c063c81ccb685ac1751f98a76
kyler.jeon <kyler.jeon@kinemaster.com>
2022-01-21 02:16:18 +0900

    Episode #167 - SwiftUI Navigation: Links, Part 3

--- SwiftUINavigation/SwiftUINavigation/ItemView.swift
+++ SwiftUINavigation/SwiftUINavigation/ItemView.swift
@@ -1,36 +1,156 @@
 import CasePaths
 import SwiftUI
 
-struct ItemView: View {
-  @Binding var item: Item
-
+struct ColorPickerView: View {
+  @ObservedObject var viewModel: ItemViewModel
+//  @Binding var color: Item.Color?
+//  @State var newColors: [Item.Color] = []
+  @Environment(\.dismiss) var dismiss
+  
   var body: some View {
     Form {
-      TextField("Name", text: self.$item.name)
-
-      Picker(selection: self.$item.color, label: Text("Color")) {
-        Text("None")
-          .tag(Item.Color?.none)
-
-        ForEach(Item.Color.defaults, id: \.name) { color in
-          Text(color.name)
-            .tag(Optional(color))
-        }
-      }
-      
-      IfCaseLet(self.$item.status, pattern: /Item.Status.inStock) { $quantity in
-        Section(header: Text("In stock")) {
-          Stepper("Quantity: \(quantity)", value: $quantity)
-          Button("Mark as sold out") {
-            self.item.status = .outOfStock(isOnBackOrder: false)
+      Button(action: {
+        self.viewModel.item.color = nil
+        self.dismiss()
+      }) {
+        HStack {
+          Text("None")
+          Spacer()
+          if self.viewModel.item.color == nil {
+            Image(systemName: "checkmark")
           }
         }
       }
-      IfCaseLet(self.$item.status, pattern: /Item.Status.outOfStock) { $isOnBackOrder in
+      
+      Section(header: Text("Default colors")) {
+        ForEach(Item.Color.defaults, id: \.name) { color in
+          Button(action: {
+            self.viewModel.item.color = color
+            self.dismiss()
+          }) {
+            HStack {
+              Text(color.name)
+              Spacer()
+              if self.viewModel.item.color == color {
+                Image(systemName: "checkmark")
+              }
+            }
+          }
+        }
+      }
+      
+      if !self.viewModel.newColors.isEmpty {
+        Section(header: Text("New colors")) {
+          ForEach(self.viewModel.newColors, id: \.name) { color in
+            Button(action: {
+              self.viewModel.item.color = color
+              self.dismiss()
+            }) {
+              HStack {
+                Text(color.name)
+                Spacer()
+                if self.viewModel.item.color == color {
+                  Image(systemName: "checkmark")
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    .task {
+      await self.viewModel.loadColors()
+    }
+  }
+}
+
+class ItemViewModel: Identifiable, ObservableObject {
+  @Published var item: Item
+  @Published var nameIsDuplicate = false
+  @Published var newColors: [Item.Color] = []
+  @Published var route: Route?
+
+  var id: Item.ID { self.item.id }
+
+  enum Route {
+    case colorPicker
+  }
+
+  init(item: Item, route: Route? = nil) {
+    self.item = item
+    self.route = route
+
+    Task { @MainActor in
+      for await item in self.$item.values {
+        await Task.sleep(NSEC_PER_MSEC * 300)
+        self.nameIsDuplicate = item.name == "Keyboard"
+      }
+    }
+  }
+
+  @MainActor
+  func loadColors() async {
+    await Task.sleep(NSEC_PER_MSEC * 500)
+    self.newColors = [
+      .init(name: "Pink", red: 1, green: 0.7, blue: 0.7),
+    ]
+  }
+
+  func setColorPickerNavigation(isActive: Bool) {
+    self.route = isActive ? .colorPicker : nil
+  }
+}
+
+struct ItemView: View {
+//  @Binding var item: Item
+  @ObservedObject var viewModel: ItemViewModel
+//  @State var nameIsDuplicate = false
+
+  var body: some View {
+    Form {
+      TextField("Name", text: self.$viewModel.item.name)
+        .background(self.viewModel.nameIsDuplicate ? Color.red.opacity(0.1) : Color.clear)
+//        .onChange(of: self.viewModel.item.name) { newName in
+//          // TODO: Validation logic
+//          Task { @MainActor in
+//            await Task.sleep(NSEC_PER_MSEC * 300)
+//            self.nameIsDuplicate = newName == "Keyboard"
+//          }
+//        }
+
+      NavigationLink(
+        unwrap: self.$viewModel.route,
+        case: /ItemViewModel.Route.colorPicker,
+        onNavigate: self.viewModel.setColorPickerNavigation(isActive:),
+        destination: { _ in ColorPickerView(viewModel: self.viewModel) }
+      ) {
+        HStack {
+          Text("Color")
+          Spacer()
+          if let color = self.viewModel.item.color {
+            Rectangle()
+              .frame(width: 30, height: 30)
+              .foregroundColor(color.swiftUIColor)
+              .border(Color.black, width: 1)
+          }
+          Text(self.viewModel.item.color?.name ?? "None")
+            .foregroundColor(.gray)
+        }
+      }
+
+      IfCaseLet(self.$viewModel.item.status, pattern: /Item.Status.inStock) { $quantity in
+        Section(header: Text("In stock")) {
+          Stepper("Quantity: \(quantity)", value: $quantity)
+          Button("Mark as sold out") {
+            self.viewModel.item.status = .outOfStock(isOnBackOrder: false)
+          }
+        }
+      }
+      IfCaseLet(self.$viewModel.item.status, pattern: /Item.Status.outOfStock) { $isOnBackOrder in
         Section(header: Text("Out of stock")) {
           Toggle("Is on back order?", isOn: $isOnBackOrder)
           Button("Is back in stock!") {
-            self.item.status = .inStock(quantity: 1)
+            self.viewModel.item.status = .inStock(quantity: 1)
           }
         }
       }
@@ -39,17 +159,13 @@ struct ItemView: View {
 }
 
 struct ItemView_Previews: PreviewProvider {
-  struct WrapperView: View {
-    @State var item = Item(name: "", color: nil, status: .inStock(quantity: 1))
-    
-    var body: some View {
-      ItemView(item: self.$item)
-    }
-  }
-  
   static var previews: some View {
     NavigationView {
-      WrapperView()
+      ItemView(
+        viewModel: .init(
+          item: Item(name: "", color: nil, status: .inStock(quantity: 1))
+        )
+      )
     }
   }
 }
--- SwiftUINavigation/SwiftUINavigation/SwiftUINavigationApp.swift
+++ SwiftUINavigation/SwiftUINavigation/SwiftUINavigationApp.swift
@@ -19,7 +19,15 @@ struct SwiftUINavigationApp: App {
               .init(item: Item(name: "Phone", color: .green, status: .outOfStock(isOnBackOrder: true))),
               .init(item: Item(name: "Headphones", color: .green, status: .outOfStock(isOnBackOrder: false))),
             ],
-            route: .row(id: keyboard.id, route: .duplicate(editedKeyboard.duplicate()))
+            route: .row(
+              id: keyboard.id,
+              route: .duplicate(
+                .init(
+                  item: editedKeyboard.duplicate(),
+                  route: .colorPicker
+                )
+              )
+            )
           ),
           selectedTab: .inventory
         )
